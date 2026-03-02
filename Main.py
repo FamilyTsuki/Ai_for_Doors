345285d42eef84de88abf0a89ba924ec7d633d72
FamilyTsuki <alban.elie590@gmail.com>
2026-03-02 22:57:52 +0100

    antrainer un desieme object

--- Main.py
+++ Main.py
@@ -1,16 +1,16 @@
 from ultralytics import YOLO
 
-from ultralytics import YOLO
-
 if __name__ == '__main__':
     model = YOLO("yolo11n.pt")
 
+
     model.train(
         data="data.yaml", 
-        epochs=40,
-        imgsz=320,
+        epochs=40,     
+        imgsz=320,      
         device="cpu", 
-        batch=12,
-        plots=True 
+        batch=12,       
+        plots=True,
+        workers=4,
+        name="train_doors_and_locks"
     )
-      
