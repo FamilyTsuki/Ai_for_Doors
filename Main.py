5e40bdb31044655f69beda36871423dd0d12ae84
FamilyTsuki <alban.elie590@gmail.com>
2026-03-02 21:31:07 +0100

    rine

--- Main.py
+++ Main.py
@@ -3,15 +3,13 @@ from ultralytics import YOLO
 from ultralytics import YOLO
 
 if __name__ == '__main__':
-    # On repart du modèle de base
     model = YOLO("yolo11n.pt")
 
-    # On lance l'entraînement
     model.train(
         data="data.yaml", 
-        epochs=40,      # Un peu plus pour être sûr
-        imgsz=320,     # Garde 320 pour ton CPU
+        epochs=40,
+        imgsz=320,
         device="cpu", 
-        batch=12,       # Petit batch pour ne pas saturer ta RAM
-        plots=True     # Pour voir les graphiques de progression
+        batch=12,
+        plots=True 
     )
