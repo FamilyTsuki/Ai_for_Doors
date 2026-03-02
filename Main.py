c71317be4c5ffa6614f02b11ce18161893c175e9
FamilyTsuki <alban.elie590@gmail.com>
2026-03-02 15:32:18 +0100

    first teatche

--- Main.py
+++ Main.py
@@ -1,13 +1,4 @@
 from ultralytics import YOLO
 import cv2
-
-# 1. Charger le modèle (il se télécharge au premier lancement)
-model = YOLO('yolov8n.pt') 
-
-# 2. Exécuter la détection sur une image de test
-results = model('https://ultralytics.com/images/bus.jpg')
-
-# 3. Afficher les résultats
-for result in results:
-    result.show()  # Ouvre une fenêtre avec l'image annotée
-
+import torch
+print("GPU disponible :" , torch.cuda.is_available())
